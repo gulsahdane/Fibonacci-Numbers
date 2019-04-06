f22709455df8e3b0479826195db2cc7c0c3b9a5d
gulsahdane <gulsahdane@gmail.com>
2019-04-06 23:28:08 +0300

    Fibonacci

---
+++ FibonacciEnumerator/FibonacciEnumerator/Class1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+
+namespace Kareler
+{
+    class Program
+    {
+        static void xxMain(string[] args)
+        {
+            Kareler f = new Kareler();
+            foreach (int n in f)
+            {
+                Console.WriteLine(n);
+                System.Threading.Thread.Sleep(200);
+            }
+            Console.ReadKey();
+        }
+    }
+
+    class Kareler : IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            return new KareEnumerator();
+        }
+    }
+
+    class KareEnumerator : IEnumerator
+    {
+        private int currentNumber;
+
+        public KareEnumerator()
+        {
+            Reset();
+        }
+        public object Current { get { return currentNumber * currentNumber; } }
+
+        public bool MoveNext()
+        {
+            ++currentNumber;
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            currentNumber = 0;
+        }
+    }
+}
